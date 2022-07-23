6dc3105d4e0105e607b8e8a66611e0631293178c
Langeloo <miguelitoramirez02.2@gmail.com>
2022-07-22 21:56:32 -0500

    CRUD

--- app_data/models.py
+++ app_data/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class organism(models.Model):
-    identificator = models.CharField(max_length=100, primary_key=True)
+class Organism(models.Model):
+    identificator = models.IntegerField(unique=True)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    string = models.CharField(max_length=60)
-    image = models.ImageField(upload_to='organism_images', blank=True)
+    description = models.CharField(max_length=500)
+    sequence = models.CharField(max_length=60)
+    image = models.ImageField(upload_to='organism_images/', blank=True, null=True)
     
     def __str__(self):
         return '{}-{}'.format(
             self.identificator,
             self.name,
         )
+    
+    def length_secuence(self):
+        return len(self.secuence)
--- app_data/urls.py
+++ app_data/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'app_data'
+app_name = "app_data"
 
 urlpatterns = [
-    path('enter-data/', views.enter_data, name='enter-data'),
-    path('list-data/', views.list_data, name='list-data'),
+    path("enter-data/", views.enter_data, name="enter-data"),
+    path("list-data/", views.list_data, name="list-data"),
+    path("update-organism/<int:identificator>/<str:action>/",
+        views.update_organism,
+        name="update-organism",
+    ),
 ]
