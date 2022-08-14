70dec9a9917bdede959aa5f37eec9c9164690c60
Langeloo <miguelitoramirez02.2@gmail.com>
2022-08-14 17:29:19 -0500

    add interface and javascript in proy

--- static/js/app_align/choices.js
+++ static/js/app_align/choices.js
@@ -12,12 +12,21 @@ var fasta_input = document.getElementById('fasta_input');
 var alert_select = document.getElementById('alert_select');
 var alert_select2 = document.getElementById('alert_select2');
 
+// data
+var data1_input = document.getElementById('data1_input');
+var bd_choice = document.getElementById('bd_choice');
+
+// container
+var config_align_container = document.getElementById('config_align_container');
+
 // add event listener to manual_radio
 manual_radio.addEventListener('change', function () {
     if (manual_radio.checked) {
         manual_input.style.display = 'block';
         fasta_input.style.display = 'none';
         alert_select.style.display = 'none';
+        data1_input.style.display = 'block';
+        config_align_container.style.display = 'block';
         console.log('manual_radio');
     }
 });
@@ -27,6 +36,24 @@ fasta_radio.addEventListener('change', function () {
         manual_input.style.display = 'none';
         fasta_input.style.display = 'block';
         alert_select.style.display = 'none';
+        data1_input.style.display = 'block';
+        config_align_container.style.display = 'block';
         console.log('fasta_radio');
+    }
+});
+
+bd_radio.addEventListener('change', function () {
+    if (bd_radio.checked) {
+        alert_select2.style.display = 'none';
+        bd_choice.style.display = 'block';
+        console.log('bd_radio');
+    }
+});
+
+seq_radio.addEventListener('change', function () {
+    if (seq_radio.checked) {
+        alert_select2.style.display = 'none';
+        bd_choice.style.display = 'none';
+        console.log('seq_radio');
     }
 });
